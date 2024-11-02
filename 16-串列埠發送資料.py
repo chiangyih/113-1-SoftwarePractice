c3c878f43acce860c85ead70a1572cfad181bb76
曾鏹毅 <tseng@ms.hhvs.tn.edu.tw>
2024-11-02 12:03:44 +0800

    Update 16-串列埠發送資料.py

--- 16-串列埠發送資料.py
+++ 16-串列埠發送資料.py
@@ -2,3 +2,28 @@
 # pip install pyserial
 
 import serial
+import time
+import sys
+
+#初始化序列通訊埠
+port="COM4" #依照狀況修改COM4
+baudrate=9600 #鮑率9600
+
+seri = serial.Serial(port, baudrate)  # 初始化序列通訊埠
+
+try:
+    while True:
+        # 終端機輸入資料
+        data = input("請輸入資料：")
+        # 送出資料至序列通訊埠
+        seri.write(data.encode())
+        print("資料已送出：", data)
+
+except (KeyboardInterrupt, serial.SerialException):
+    print("找不到序列通訊埠或通訊埠被佔用！")
+
+finally:
+    seri.close()
+    print("序列通訊埠已關閉")
+    
+
